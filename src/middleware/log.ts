853dbc3833c208933946f3735f44d788916dd612
Sebastian Toledo <seba95matias@hotmail.com>
2023-04-18 13:48:05 -0300

    deleact console log

--- src/middleware/log.ts
+++ src/middleware/log.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
 const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  console.log("hola soy el LOG");
   next();
 };
 
